10f95604ea190e98191b4744db62a29a92f7498c
Dominik Okoń <dominikokon3@gmail.com>
2022-04-26 10:43:35 +0200

    Ciąg Fibbonaciego Dominik Okoń 3E

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int a;
+    cout << "Jaką ilość ciągu wyrazów chcesz wypisać: ";
+    cin >> a;
+
+    for (int i = 0; i < a; i++)
+    {
+        cout << fibonacci(i) << endl;
+    }
+}
